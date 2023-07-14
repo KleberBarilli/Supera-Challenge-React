e299560afd76a23abc34247dd60feb5cea604917
kleber.barilli <kleber.barilli73@gmail.com>
2023-07-14 14:18:52 -0300

    estilização com o tailwind

--- src/core/TransferenciasTablePagination.tsx
+++ src/core/TransferenciasTablePagination.tsx
@@ -1,6 +1,7 @@
 import { ISearch } from "@/domain/types/Search";
 import { Transferencia } from "@/domain/types/Transferencia";
 import { getTransferencias } from "@/pages/api";
+import { dateFormatToDDMMYYYY } from "@/utils/dateFormat";
 import React, { useEffect, useState } from "react";
 
 export const TransferenciasTablePagination: any = () => {
@@ -40,7 +41,7 @@ export const TransferenciasTablePagination: any = () => {
 
   const pageNumbers = [0];
 
-  for (let i = 1; i <= totalPages; i++) {
+  for (let i = 1; i < totalPages; i++) {
     pageNumbers.push(i);
   }
   useEffect(() => {
@@ -70,32 +71,39 @@ export const TransferenciasTablePagination: any = () => {
 
   return (
     <div>
-      <div>
-        <label>Data de ínicio</label>
-        <input
-          type="date"
-          className="form-input text-black"
-          onChange={(e) => setDataInicio(e.target.value)}
-          value={dataInicio}
-        />
-        <label>Data de Fim</label>
-        <input
-          type="date"
-          className="form-input text-black"
-          onChange={(e) => setDataFim(e.target.value)}
-          value={dataFim}
-        />
-        <label>Nome operador transacionado</label>
-        <input
-          type="text"
-          className="form-input text-black"
-          onChange={(e) => setNomeOperadorTransacionado(e.target.value)}
-          value={nomeOperadorTransacionado}
-        />
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <label>Data de ínicio</label>
+          <input
+            type="date"
+            className="form-input text-black"
+            onChange={(e) => setDataInicio(e.target.value)}
+            value={dataInicio}
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label>Data de Fim</label>
+          <input
+            type="date"
+            className="form-input text-black"
+            onChange={(e) => setDataFim(e.target.value)}
+            value={dataFim}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label>Nome operador transacionado</label>
+          <input
+            type="text"
+            className="form-input text-black"
+            onChange={(e) => setNomeOperadorTransacionado(e.target.value)}
+            value={nomeOperadorTransacionado}
+          />
+        </div>
       </div>
       <div className="flex justify-end w-full my-16">
         <button
-          className="py-1.5 flex justify-center items-center px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-cyan-500 hover:bg-cyan-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-600"
+          className="py-1.5 flex justify-center items-center px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-400 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-600"
           onClick={() =>
             search({
               dataInicio: new Date(dataInicio),
@@ -108,30 +116,49 @@ export const TransferenciasTablePagination: any = () => {
         </button>
       </div>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Saldo Total: R${saldoTotal}</th>
-            <th>Saldo no período: R${saldoNoPeriodo}</th>
-          </tr>
-          <tr>
-            <th>Dados</th>
-            <th>Valentia</th>
-            <th>Tipo</th>
-            <th>Nome do operador transacionado</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transferencias.map((transferencia) => (
-            <tr key={transferencia.id}>
-              <td>{transferencia.dataTransferencia}</td>
-              <td>{transferencia.valor}</td>
-              <td>{transferencia.tipo}</td>
-              <td>{transferencia.nomeOperadorTransacao}</td>
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead>
+            <tr>
+              <th className="py-3 px-4 text-left border border-black">
+                Saldo Total: R${saldoTotal}
+              </th>
+              <th className="py-3 px-4 text-left border border-black">
+                Saldo no período: R${saldoNoPeriodo}
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            <tr>
+              <th className="py-3 px-4 text-left border border-black">Dados</th>
+              <th className="py-3 px-4 text-left border border-black">
+                Valentia
+              </th>
+              <th className="py-3 px-4 text-left border border-black">Tipo</th>
+              <th className="py-3 px-4 text-left border border-black">
+                Nome do operador transacionado
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {transferencias &&
+              transferencias.map((transferencia) => (
+                <tr key={transferencia.id}>
+                  <td className="py-3 px-4 border border-black">
+                    {dateFormatToDDMMYYYY(transferencia.dataTransferencia)}
+                  </td>
+                  <td className="py-3 px-4 border border-black">
+                    {transferencia.valor}
+                  </td>
+                  <td className="py-3 px-4 border border-black">
+                    {transferencia.tipo}
+                  </td>
+                  <td className="py-3 px-4 border border-black">
+                    {transferencia.nomeOperadorTransacao}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
 
       <div>
         {pageNumber > 0 && (
@@ -143,7 +170,7 @@ export const TransferenciasTablePagination: any = () => {
             onClick={() => setPageNumber(item)}
             aria-current="page"
             hidden={isLast}
-            className={`relative z-10 inline-flex items-center cursor-pointer ${
+            className={`relative z-10 inline-flex items-center cursor-pointer px-4 ${
               pageNumber === item
                 ? "bg-black text-white"
                 : "bg-white text-black border-white hover:bg-white focus:z-20 focus:outline-offset-0 ring-1 ring-inset ring-white"
@@ -153,8 +180,14 @@ export const TransferenciasTablePagination: any = () => {
           </span>
         ))}
 
-        {pageNumber < Math.ceil(transferencias.length / totalPages) && (
-          <button onClick={() => setPageNumber(pageNumber + 1)}>{">"}</button>
+        {pageNumber <
+          Math.ceil(transferencias && transferencias.length / totalPages) && (
+          <button
+            className="px-2"
+            onClick={() => setPageNumber(pageNumber + 1)}
+          >
+            {">"}
+          </button>
         )}
       </div>
     </div>
--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -5,3 +5,11 @@ export const dateFormat = (date: Date) => {
 
   return `${ano}-${mes}-${dia}`;
 };
+export const dateFormatToDDMMYYYY = (date: string) => {
+  const partes = date.split("-");
+  const dia = partes[2];
+  const mes = partes[1];
+  const ano = partes[0];
+
+  return `${dia}/${mes}/${ano}`;
+};
